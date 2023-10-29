1effb977bfa755f1c6969c38cc10418ea0bcc835
LFWB <griefsoft@protonmail.com>
2023-10-29 14:32:42 +0200

    ragmod init fix

--- lua/autorun/sh_ragmod.lua
+++ lua/autorun/sh_ragmod.lua
@@ -2,11 +2,12 @@ include("ragmod/ragmod_utils.lua")
 include("ragmod/ragmod_options.lua")
 include("ragmod/ragmod.lua")
 
+AddCSLuaFile("ragmod/ragmod_input.lua")
+include("ragmod/ragmod_input.lua")
+
 if SERVER then
 	include("ragmod/sv_ragmod.lua")
 	AddCSLuaFile("ragmod/cl_ragmod.lua")
-	AddCSLuaFile("ragmod/ragmod_input.lua")
 else
 	include("ragmod/cl_ragmod.lua")
-	include("ragmod/ragmod_input.lua")
 end
