baee35cbff066efa80500f468b591f7403855bcd
LFWB <griefsoft@protonmail.com>
2023-10-31 18:40:58 +0200

    custom lua fix

--- lua/autorun/sh_ragmod.lua
+++ lua/autorun/sh_ragmod.lua
@@ -1,5 +1,10 @@
+AddCSLuaFile("ragmod/ragmod_utils.lua")
 include("ragmod/ragmod_utils.lua")
+
+AddCSLuaFile("ragmod/ragmod_options.lua")
 include("ragmod/ragmod_options.lua")
+
+AddCSLuaFile("ragmod/ragmod.lua")
 include("ragmod/ragmod.lua")
 
 AddCSLuaFile("ragmod/ragmod_input.lua")
